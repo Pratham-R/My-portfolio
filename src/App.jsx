3fda6d126409c9e827a0b5ba316a3220a003905f
Pratham Rastogi <85737561+Pratham-R@users.noreply.github.com>
2023-06-16 16:26:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Pratham Rastoi",
+  name: "Pratham Rastogi",
   title: "Web Designer & Developer",
   email: "prathamrastogi5@gmail.com",
   gitHub: "https://github.com/Pratham-R/",
